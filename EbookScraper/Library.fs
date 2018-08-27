f12d438c0b8357ee64c46024763a5c14209a3867
J2ghz <j2.00ghz@gmail.com>
2018-08-27 21:40:56 +0200

    fixed

--- EbookScraper/Library.fs
+++ EbookScraper/Library.fs
@@ -3,45 +3,12 @@ namespace EbookScraper.Common
 open FSharp.Data
 module Scraper =
 
-    type Selector = string
-
     type Url = string
-
-    type Processor =
-    | Root of Url
-    | Index of Selector
-    | Content of Selector
 
     type Output =
     | Link of Url * string
     | Page of HtmlDocument
     | ConcatContent of string
-
-    let toProcessors argv =
-        Root (List.head argv)::(argv|> List.tail|> List.rev |> List.tail |> List.rev |> List.map (fun str -> Index str) |> List.append [Content (List.last argv)] )
-
-    let load link = HtmlDocument.Load(link.url)
-
-    let load' links =
-        List.map load links
-
-    let extractText (node:HtmlNode) =
-        {
-            url = node.AttributeValue("href");
-            name = node.Name();
-        }
-
-    let scrape selector (document:HtmlDocument) =
-        document.CssSelect(selector)
-        |> List.map extractText
-
-    let scrape' selector documents = List.collect (scrape selector) documents
-
-    let rec scrape'' selectors documents =
-        match selectors with
-        | [selector] -> scrape' selector documents |> List.fold (fun state link -> state + link.name) ""
-        | selector::tail -> scrape' selector documents |> load' |> scrape'' tail
-        | [] -> ""
 
     let getUrlorText (n:HtmlNode) =
         if n.AttributeValue("href") <> "" then
@@ -49,8 +16,8 @@ module Scraper =
         else
             ConcatContent (n.InnerText())
 
-    let rec scrape''' selectors o =
+    let rec scrape selectors o =
         match o with
-        | Link (url,name) -> scrape''' selectors (Page (HtmlDocument.Load url))
-        | Page page -> page.CssSelect(List.head selectors) |> List.map getUrlorText |> List.collect (scrape''' (List.tail selectors))
+        | Link (url,name) -> (sprintf "# %s \n" name) :: scrape selectors (Page (HtmlDocument.Load url))
+        | Page page -> page.CssSelect(List.head selectors) |> List.map getUrlorText |> List.collect (scrape (List.tail selectors))
         | ConcatContent s -> [s]
--- EbookScraper/Program.fs
+++ EbookScraper/Program.fs
@@ -9,11 +9,7 @@ open System.Text
 let main argv' =
     let argv = Array.toList argv'
     let url = argv |> List.head
-    let content = argv |> List.rev |> List.head
-    let indexes = argv |> List.tail |> List.rev |> List.tail |> List.rev
-    let root = Scraper.load {url=url;name="Root"}
-    //let lastIndexes = List.fold (fun docs selector -> Scraper.scrape' selector docs |> Scraper.load') [root] indexes
-    //let content = Scraper.scrape' content lastIndexes |> List.fold (fun total str -> total + str.name + " \n")  ""
-    let content = Scraper.scrape'' argv.Tail [root]
-    System.IO.File.WriteAllText("book.txt",content)    
+    let indexes = argv |> List.tail
+    let content = Scraper.scrape indexes (Scraper.Output.Link (url,""))
+    System.IO.File.WriteAllLines("book.txt",content)
     0
