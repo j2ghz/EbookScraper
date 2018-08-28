55df4cb39d867cb7a9b0d90b68cdb6ea9ba7c3c2
J2ghz <j2.00ghz@gmail.com>
2018-08-28 10:41:01 +0200

    combine files

--- EbookScraper/Program.fs
+++ EbookScraper/Program.fs
@@ -1,15 +1,33 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open EbookScraper.Common
 open FSharp.Data
 open System.Text
+
+type Url = string
+
+type Output =
+| Link of Url * string
+| Page of HtmlDocument
+| ConcatContent of string
+
+let getUrlorText (n:HtmlNode) =
+    if n.AttributeValue("href") <> "" then
+        Link (n.AttributeValue("href"), n.InnerText())
+    else
+        ConcatContent (n.InnerText())
+
+let rec scrape selectors o =
+    match o with
+    | Link (url,name) -> (sprintf "# %s \n" name) :: scrape selectors (Page (HtmlDocument.Load url))
+    | Page page -> page.CssSelect(List.head selectors) |> List.map getUrlorText |> List.collect (scrape (List.tail selectors))
+    | ConcatContent s -> [s]
 
 [<EntryPoint>]
 let main argv' =
     let argv = Array.toList argv'
     let url = argv |> List.head
     let indexes = argv |> List.tail
-    let content = Scraper.scrape indexes (Scraper.Output.Link (url,""))
+    let content = scrape indexes (Output.Link (url,""))
     System.IO.File.WriteAllLines("book.txt",content)
     0
